3a793ea5233b27d36be9f802a2295096d59cb2bc
Bruno H. Homem <brunohhomem@gmail.com>
2024-03-12 07:51:44 -0300

    first commit

---
+++ regAluno-backend/src/main/java/com/bhh/RegistroAlunos/RegistroAlunosApplication.java
@@ -0,0 +1,13 @@
+package com.bhh.RegistroAlunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroAlunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroAlunosApplication.class, args);
+	}
+
+}
