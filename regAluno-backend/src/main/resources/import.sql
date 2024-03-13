e2899980becaab02e71646eb08be8c8e5a6c3e3a
Bruno H. Homem <brunohhomem@gmail.com>
2024-03-13 18:28:36 -0300

    added: import.sql

---
+++ regAluno-backend/src/main/resources/import.sql
@@ -0,0 +1,13 @@
+INSERT INTO cursos (nome, inativo, createdat) VALUES ('Arquitetura', false, '2022-01-01 00:00:00');
+INSERT INTO cursos (nome, inativo, createdat) VALUES ('Biomedicina', false, '2023-11-05 00:00:00');
+INSERT INTO cursos (nome, inativo, createdat) VALUES ('Medicina', true, '2024-10-11 00:00:00');
+INSERT INTO cursos (nome, inativo, createdat) VALUES ('Direito', true, '2022-01-01 00:00:00');
+INSERT INTO cursos (nome, inativo, createdat) VALUES ('Psicologia', false, '2021-01-11 00:00:00');
+
+INSERT INTO alunos (nome, matricula, cursos, data_nasc, createdat) VALUES ('Bruno', '112233', 'Biomedicina', '1994-06-14', '2017-01-10 00:00:00');
+
+INSERT INTO alunos (nome, matricula, cursos, data_nasc, createdat) VALUES ('Vitória', '332211', 'Psicologia', '1997-01-10','2017-01-10 00:00:00');
+INSERT INTO alunos (nome, matricula, cursos, data_nasc, createdat) VALUES ('Butters', '222222', 'Direito', '2019-01-10','2017-01-10 00:00:00');
+INSERT INTO alunos (nome, matricula, cursos, data_nasc, createdat) VALUES ('Mione', '111111', 'Medicina', '2023-01-10','2017-01-10 00:00:00');
+
+
